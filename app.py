52675a60084888cb6ae0cb73ef5894c61c13d6f7
Chardenet Martin <ba191970@ens.uqam.ca>
2024-09-10 15:38:15 -0400

    Ajout de app.py et debut de base.html

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/array')
+def array():
+    return render_template('array.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
